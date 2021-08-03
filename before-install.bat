9cf5a46a9c4b21bc72786dca1769af3c77148095
HSJ-89 <harsha.sj89@gmail.com>
2021-08-03 14:16:51 +0400

    change

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,2 @@
-REM Install Internet Information Server (IIS).
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
+
 Del c:\inetpub\wwwroot\index.html
